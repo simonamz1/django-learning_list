f1da098262ae434512d9be653d80e117d1565bb9
simonamz <simonamzalag@gmail.com>
2018-01-24 11:13:26 +0100

    S2V3: Question and Answer Models (adding the 2 new models)

--- learning_site/courses/admin.py
+++ learning_site/courses/admin.py
@@ -1,18 +1,10 @@
 from django.contrib import admin
 
-from .models import Course, Text, Quiz
+from . import models
 
 
-class TextInline(admin.StackedInline):
-    model = Text
-
-class QuizInline(admin.StackedInline):
-    model = Quiz
-
-class CourseAdmin(admin.ModelAdmin):
-    inlines = [TextInline, QuizInline,]
-
-
-admin.site.register(Course, CourseAdmin)
-admin.site.register(Text)
-admin.site.register(Quiz)
+admin.site.register(models.Course)
+admin.site.register(models.Text)
+admin.site.register(models.Quiz)
+admin.site.register(models.Question)
+admin.site.register(models.Answer)
